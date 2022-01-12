55fe82a9529e7c34ef96115e1e01babe6a2d725f
11asa11 <serega_kozm@mail.ru>
2022-01-12 18:28:26 +0300

    finally

--- LAB_1_1.py
+++ LAB_1_1.py
@@ -2,8 +2,12 @@ from math import log, exp, factorial
 import random
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from scipy.stats import chi2
+
+def toFixed(numObj, digits=0):
+    return f"{numObj:.{digits}f}"
 
 class Lab():
     def __init__(self):
@@ -54,17 +58,17 @@ class Lab():
 
             print("x:  ", end =" ")
             for x in self.array_of_x:
-                print(round(x, count_of_symb), end =" ")
+                print(toFixed(x, count_of_symb), end =" ")
             print()
 
             print("y:  ", end =" ")
             for y in self.array_of_y:
-                print(round(y, count_of_symb), end =" ")
+                print(toFixed(y, count_of_symb), end =" ")
             print()
 
             print("tau:", end =" ")
             for tau in self.array_of_tau:
-                print(round(tau, count_of_symb), end =" ")
+                print(toFixed(tau, count_of_symb), end =" ")
             print()
 
     def sort_y(self):
@@ -104,6 +108,11 @@ class Lab():
         result = abs(f_x_zero - new_f_x_zero)
         print("Ответ: " + str(round(result, 10)))
         print()
+
+    def clean_arrays_for_second_part(self):
+        self.array_of_z.clear()
+        self.array_of_l.clear()
+        self.array_of_f.clear()
 
     def first_case_of_bins(self):
         """
@@ -151,7 +160,7 @@ class Lab():
             z_average.insert( iter, round((self.array_of_z[i] + self.array_of_z[i+1])/2, 2) )
             iter += 1
 
-        print("z_average: " + str(z_average))
+        #print("z_average: " + str(z_average))
         return z_average
 
     def compute_f_ksi_z(self, z_average):
@@ -162,11 +171,11 @@ class Lab():
             f_ksi.insert(iter, f_ksi_i)
             iter += 1
 
-        print("f_ksi: " + str(f_ksi))
+        #print("f_ksi: " + str(f_ksi))
         return f_ksi
 
     def print_arrays_for_second_part(self):
-        print("y: [", end="")
+        #print("y: [", end="")
 
         y_iter = 0
         for y in self.array_of_y:
@@ -182,9 +191,17 @@ class Lab():
         print("l: " + str(self.array_of_l))
         print("f: " + str(self.array_of_f))
 
+    def compute_mery(self, f_ksi_z):
+        mera = float(0)
+
+        for i in range(len(self.array_of_l)):
+            mera += (f_ksi_z[i] - self.array_of_f[i])**2
+
+        print("Мера схожести: " + str(round(mera, 5)))
+
     def create_histogram_first_case(self):
         param_m = int(1.44 * log(self.param_n) + 1)
-        print(param_m)
+        #print(param_m)
         bins = plt.hist(self.array_of_y, bins = param_m, density=True, range = (0,max(self.array_of_y)))
         #sns.histplot(data=self.array_of_y, bins=param_m, stat='density')
         iter = 0
@@ -194,52 +211,91 @@ class Lab():
 
         self.compute_l_array()
         self.compute_f_array()
-        self.print_arrays_for_second_part()
+        #self.print_arrays_for_second_part()
 
         z_average = self.compute_z_average()
-        plt.plot(z_average, self.compute_f_ksi_z(z_average))
+        f_ksi_z = self.compute_f_ksi_z(z_average)
+        plt.plot(z_average, f_ksi_z, color = "red")
+
+        self.compute_mery(f_ksi_z)
+
+        plt.show()
+
+    def create_histogram_second_case(self):
+        param_m = int(1.44 * log(self.param_n) + 1)
+        bins_razr = np.interp(np.linspace(0, self.param_n, param_m + 1),
+                         np.arange(self.param_n),
+                         np.sort(self.array_of_y))
+        #print(bins_razr)
+        bins = plt.hist(self.array_of_y, bins=bins_razr, density=True)
+
+        iter = 0
+        for ranges in bins[1]:
+            self.array_of_z.insert(iter, round(ranges, 2))
+            iter += 1
+
+        self.compute_l_array()
+        self.compute_f_array()
+
+        z_average = self.compute_z_average()
+        f_ksi_z = self.compute_f_ksi_z(z_average)
+        plt.plot(z_average, f_ksi_z, color = "red")
+
+        self.compute_mery(f_ksi_z)
 
         plt.show()
 
     def create_histogram_fourth_case(self):
-        plt.hist(self.array_of_y, bins = self.array_of_z)
+        plt.hist(self.array_of_y, bins = self.array_of_z, density=True)
 
         z_average = self.compute_z_average()
-        plt.plot(z_average, self.compute_f_ksi_z(z_average))
+        f_ksi_z = self.compute_f_ksi_z(z_average)
+        plt.plot(z_average, f_ksi_z, color = "red")
+
+        self.compute_mery(f_ksi_z)
 
         plt.show()
 
     def second_part_first_case(self):
+        self.clean_arrays_for_second_part()
+
         self.first_case_of_bins()
         self.create_histogram_first_case()
 
+    def second_part_second_case(self):
+        self.clean_arrays_for_second_part()
+
+        self.create_histogram_second_case()
+
     def second_part_fourth_case(self):
+        self.clean_arrays_for_second_part()
+
         self.fourth_case_of_bins()
         self.compute_l_array()
         self.compute_f_array()
-        self.print_arrays_for_second_part()
+        #self.print_arrays_for_second_part()
         self.create_histogram_fourth_case()
 
     def third_part(self):
         r = len(self.array_of_l) - 1
+        print("Рассматривается гипотеза H0 случайной велечины, распределенной по показательному распределению с")
+        print("lamda = " + str(self.param_lambda) + " и r = " + str(r) + " степенями свободы")
         R_0 = 0
-        print(r)
+        #print(r)
         for i in range(len(self.array_of_l)):
-            print("counter of i: " + str(i) + ". l[i]: " + str(self.array_of_l[i]) + ". z[i]: " + str(self.array_of_z[i]) + ". z[i+1]: " + str(self.array_of_z[i+1]))
+            #print("counter of i: " + str(i) + ". l[i]: " + str(self.array_of_l[i]) + ". z[i]: " + str(self.array_of_z[i]) + ". z[i+1]: " + str(self.array_of_z[i+1]))
             p_i = (1 - exp(-self.param_lambda * self.array_of_z[i + 1])) - (
                         1 - exp(-self.param_lambda * self.array_of_z[i]))
             R_0 += ((self.array_of_l[i] - self.param_n * p_i) ** 2) / (self.param_n * p_i)
 
-        print("ChiSquare: " + str(round(R_0, 2)))
-
-        alpha = 0.95
+        alpha = 1 - round(float(input("Квантиль распределения: ")), 2)
+        print("Число степеней свободы = " + str(r))
         critical_value = chi2.ppf(alpha, r)
-        print("Critical value: " + str(round(critical_value, 2)))
 
         if R_0 < critical_value:
-            print("Гипотеза принимается")
+            print("R0 = " + str(round(R_0, 2)) + ". Критическое значение = " + str(round(critical_value, 2)) + ". Гипотеза принимается")
         else:
-            print("Гипотеза отвергается")
+            print("R0 = " + str(round(R_0, 2)) + ". Критическое значение = " + str(round(critical_value, 2)) + ". Гипотеза отвергается")
 
     def set_t_zero(self):
         upper_limit_t_zero = round(5 / self.param_lambda, 2)
@@ -247,17 +303,17 @@ class Lab():
         self.t_zero = round(float(input((f'Введите t0 от {lower_limit_t_zero} до {upper_limit_t_zero}: '))),2)
 
     def compute_intervals(self):
-        intervals = []
+        self.intervals = []
         self.m_count_of_intervals = int(max(self.array_of_tau)/self.t_zero) # is m
 
         current_value = 0
         for i in range(int(self.m_count_of_intervals + 1)):
-            intervals.insert(i, round(current_value, 2))
+            self.intervals.insert(i, round(current_value, 2))
             current_value += self.t_zero
 
-        print("count_of_intervals: " + str(round(self.m_count_of_intervals, 2)))
-        print("intervals: " + str(intervals))
-        return intervals
+        #print("count_of_intervals: " + str(round(self.m_count_of_intervals, 2)))
+        #print("intervals: " + str(intervals))
+        return self.intervals
 
     def count_points(self, intervals):
         array_of_counts = []
@@ -269,7 +325,7 @@ class Lab():
                     count += 1
             array_of_counts.insert(i, count)
 
-        print("array_of_counts: " + str(array_of_counts))
+        #print("array_of_counts: " + str(array_of_counts))
 
         set_of_counts = set(array_of_counts)
         self.n_count_table = dict.fromkeys(set_of_counts, 0)
@@ -278,14 +334,33 @@ class Lab():
 
         self.k = max(set_of_counts)
 
-        print("self.k = " + str(self.k))
-        print("set_of_counts: " + str(set_of_counts))
-        print("dict_table: " + str(self.n_count_table))
+        #print("self.k = " + str(self.k))
+        #print("set_of_counts: " + str(set_of_counts))
+        #print("dict_table: " + str(self.n_count_table))
+
+    def fourth_part_print_table(self):
+        print("tau:      ", end=" ")
+        for tau in self.array_of_tau:
+            print(toFixed(tau, 2), end=" ")
+        print()
+
+        print("интервалы:", end=" ")
+        for val in self.intervals:
+            print(toFixed(val, 2), end=" ")
+        print()
+
+        print("[a, b]. a - количество заявок, b - число интервалов")
+        for key, value in self.n_count_table.items():
+            print("[" + str(key) + "," + str(value) + "]", end=" ")
+        print()
 
     def fourth_part_chi_square(self):
+        print("Рассматривается гипотеза H0 случайной велечины, распределенной по закону Пуассона с")
+        print("lamda*t0 = " + str(self.param_lambda * self.t_zero) + " и r = " + str(self.k) + " степенями свободы")
+
         R_0 = 0
         for i in range(self.k + 1):
-            print("counter of i: " + str(i))
+            #print("counter of i: " + str(i))
             p_i = exp(-self.param_lambda*self.t_zero) * (self.param_lambda * self.t_zero)**i * (1/(factorial(i)))
 
             n_i = 0
@@ -293,16 +368,55 @@ class Lab():
                 n_i = self.n_count_table[i]
             R_0 += ((n_i - self.m_count_of_intervals * p_i) ** 2) / (self.m_count_of_intervals * p_i)
 
-        print("ChiSquare: " + str(round(R_0, 2)))
-
-        alpha = 0.95
+        alpha = 1 - round(float(input("Квантиль распределения: ")), 2)
+        print("Число степеней свободы = " + str(self.k) + " = Наибольшее наблюдаемое количество заявок")
+        print("Количетсво интервалов (m) = " + str(self.m_count_of_intervals))
         critical_value = chi2.ppf(alpha, self.k)
-        print("Critical value: " + str(round(critical_value, 2)))
 
         if R_0 < critical_value:
-            print("Гипотеза принимается")
+            print("R0 = " + str(round(R_0, 2)) + ". Критическое значение = " + str(round(critical_value, 2)) + ". Гипотеза принимается")
         else:
-            print("Гипотеза отвергается")
+            print("R0 = " + str(round(R_0, 2)) + ". Критическое значение = " + str(round(critical_value, 2)) + ". Гипотеза отвергается")
+
+def snd_part(lab):
+    # SECOND PART
+    print()
+    print("ВТОРАЯ ЧАСТЬ")
+    lab.sort_y()
+    print("Доступные варианты выбора разрядов:")
+    print("1 - Выбор разрядов одинаковой длины")
+    print("2 - Выбор разрядов равномерно")
+    print("3 - Выбор разрядов в зависимости от точек")
+    print("4 - Выбор разрядов вручную")
+
+    exit_flag = True
+    while (exit_flag):
+        input_choice = input("Введите номер выбора разрядов: ")
+        if "1" in input_choice:
+            lab.second_part_first_case()
+        if "2" in input_choice:
+            lab.second_part_second_case()
+        if "3" in input_choice:
+            lab.check_through_func_dist()
+        if "4" in input_choice:
+            lab.second_part_fourth_case()
+
+        input_continue = input("Выбрать новый выбор разряда? 1 - Да. 0 - Нет: ")
+        if "1" in input_continue:
+            exit_flag = True
+        if "0" in input_continue:
+            exit_flag = False
+
+def fth_part(lab):
+    #FOURTH PART
+    print()
+    print("ЧЕТВЕРТАЯ ЧАСТЬ")
+    lab.set_t_zero()
+    lab.count_points(lab.compute_intervals())
+    print_tab = input("Вывести таблицу количество заявок\число интервалов? 1 - Да. 0 - Нет: ")
+    if "1" in print_tab:
+        lab.fourth_part_print_table()
+    lab.fourth_part_chi_square()
 
 def Main():
     print()
@@ -327,25 +441,28 @@ def Main():
         if "0" in input_continue:
             exit_flag = False
 
-    # SECOND PART
+
     print()
-    print("ВТОРАЯ ЧАСТЬ")
+    go_to_part = input("Перейти сразу 4 четвертой части?: 1 - Да. 0 - Нет: ")
 
-    lab.sort_y()
-    lab.second_part_first_case()
+    if "1" in go_to_part:
+        fth_part(lab)
 
-    #THIRD PART
-    print()
-    print("ТРЕТЬЯ ЧАСТЬ")
-    lab.third_part()
+        snd_part(lab)
 
-    #FOURTH PART
-    print()
-    print("ЧЕТВЕРТАЯ ЧАСТЬ")
-    lab.set_t_zero()
-    lab.count_points(lab.compute_intervals())
-    lab.fourth_part_chi_square()
+        #THIRD PART
+        print()
+        print("ТРЕТЬЯ ЧАСТЬ")
+        lab.third_part()
+    else:
+        snd_part(lab)
 
+        #THIRD PART
+        print()
+        print("ТРЕТЬЯ ЧАСТЬ")
+        lab.third_part()
+
+        fth_part(lab)
 
 if __name__ == "__main__":
     Main()
